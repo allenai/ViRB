c6dcb2d34f7d1ac4e4fb38c44adeb5eae96769d3
Klemen Kotar <klemen@kotar.us>
2020-11-16 03:46:49 -0800

    Ran another test with the full conv

--- models/ClassificationHead.py
+++ models/ClassificationHead.py
@@ -7,8 +7,8 @@ class ClassificationHead(nn.Module):
 
     def __init__(self, embedding_size, output_size):
         super().__init__()
-        # resnet = torchvision.models.resnet50(pretrained=True)
-        # self.encoder = nn.Sequential(*list(resnet.children())[:-1])
+        resnet = torchvision.models.resnet50(pretrained=True)
+        self.encoder = nn.Sequential(*list(resnet.children())[:-1])
         self.head = nn.Sequential(
             nn.Linear(embedding_size, 512),
             nn.ReLU(inplace=True),
@@ -18,6 +18,6 @@ class ClassificationHead(nn.Module):
         )
 
     def forward(self, x):
-        # with torch.no_grad():
-        #     x = self.encoder(x).view(-1, 2048)
+        with torch.no_grad():
+            x = self.encoder(x).view(-1, 2048)
         return self.head(x)
