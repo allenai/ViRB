dfaa9d0835a14808f5f2e82e65bf34f41a6822e8
Klemen Kotar <klemen@kotar.us>
2020-11-15 19:08:58 -0800

    Fixed multi leayer projector head again

--- models/ClassificationHead.py
+++ models/ClassificationHead.py
@@ -5,7 +5,7 @@ class ClassificationHead(nn.Module):
 
     def __init__(self, embedding_size, output_size):
         super().__init__()
-        nn.Sequential(
+        self.head = nn.Sequential(
             nn.Linear(embedding_size, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256),
