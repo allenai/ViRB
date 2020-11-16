e6866bbef786149720915ee176bbde48e3869ad5
Klemen Kotar <klemen@kotar.us>
2020-11-16 03:29:39 -0800

    Bumped down to lr of 0.0001

--- datasets/Caltech101EncldableDataset.py
+++ datasets/Caltech101EncldableDataset.py
@@ -42,7 +42,7 @@ class CalTech101EncodableDataset(EncodableDataset):
         model.eval()
         batch = []
         for img in tqdm.tqdm(self.data):
-            if len(batch) == 600:
+            if len(batch) == 500:
                 batch = torch.stack(batch, dim=0).to(self.device)
                 with torch.no_grad():
                     out = model(batch).detach()
