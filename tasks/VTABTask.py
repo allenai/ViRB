Fixed logging and removed gradients

tasks/VTABTask.py
@@ -74,9 +74,10 @@ class VTABTask:
         test_errors = []
         for x, label in self.test_dataloader:
             x, label = x.to(self.device), label.to(self.device)
-            out = self.model(x)
-            test_loss = self.loss(out, label)
-            test_losses.append(test_loss)
-            test_error = self.error(out, label)
-            test_errors.append(test_error.item())
+            with torch.no_grad():
+                out = self.model(x)
+                test_loss = self.loss(out, label)
+                test_losses.append(test_loss)
+                test_error = self.error(out, label)
+                test_errors.append(test_error.item())
         return np.mean(test_losses), np.mean(test_errors)