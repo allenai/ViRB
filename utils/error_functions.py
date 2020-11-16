d5f0f27464ee5094843d41e2b8cbd230cbe32dc7
Klemen Kotar <klemen@kotar.us>
2020-11-15 16:52:02 -0800

    Added a ton more logging

--- utils/error_functions.py
+++ utils/error_functions.py
@@ -4,5 +4,9 @@ import torch
 def classification_error(out, labels):
     with torch.no_grad():
         predictions = torch.argmax(out)
-        print(torch.count_nonzero(predictions == labels) / labels.size(0), labels.size(0))
+        print("raw predictions:", out, "\n",
+              "predictions:", predictions, "\n",
+              "label size:", labels.size(0), "\n",
+              "error:", torch.count_nonzero(predictions == labels) / labels.size(0), "\n"
+        )
         return torch.count_nonzero(predictions == labels) / labels.size(0)
