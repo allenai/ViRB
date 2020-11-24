bf1839ceb99678af7d5eee4b6382aaf12bf5d86b
Klemen Kotar <klemen@kotar.us>
2020-11-24 00:49:07 -0800

    Moved to a single layer classification head to try and fix the lack of repeatablility

--- models/ClassificationHead.py
+++ models/ClassificationHead.py
@@ -8,13 +8,14 @@ class ClassificationHead(nn.Module):
     def __init__(self, embedding_size, output_size):
         super().__init__()
         self.embedding_size = embedding_size
-        self.head = nn.Sequential(
-            nn.Linear(embedding_size, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, output_size)
-        )
+        # self.head = nn.Sequential(
+        #     nn.Linear(embedding_size, 256),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(256, 256),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(256, output_size)
+        # )
+        self.head = nn.Linear(embedding_size, output_size)
 
     def forward(self, x):
         return self.head(x["embedding"])
