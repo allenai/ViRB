375d06bad3127968f5a90c338d734eb393358ec3
Klemen Kotar <klemen@kotar.us>
2020-11-24 14:29:05 -0800

    Fixed error function to return item

--- utils/error_functions.py
+++ utils/error_functions.py
@@ -4,4 +4,4 @@ import torch
 def classification_error(out, labels):
     with torch.no_grad():
         _, predictions = torch.max(out, dim=1)
-        return (predictions == labels).sum().item() / labels.size(0)
+        return (predictions == labels).sum() / labels.size(0)
