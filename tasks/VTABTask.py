beeddcd50cfae8b53c9c42dfa49d97c274aba16b
Klemen Kotar <klemen@kotar.us>
2020-11-23 22:33:52 -0800

    Fixed tensorboard logging

--- tasks/VTABTask.py
+++ tasks/VTABTask.py
@@ -46,8 +46,8 @@ class VTABTask:
         for e in range(epochs):
             train_loss, train_accuracy = self.train_epoch()
             test_loss, test_accuracy = self.test()
-            writer.add_scalar("TrainLoss/"+self.name, train_loss, e)
-            writer.add_scalar("TestAccuracy/"+self.name, test_accuracy, e)
+            writer.add_scalar("TrainLoss/"+self.task, train_loss, e)
+            writer.add_scalar("TestAccuracy/"+self.task, test_accuracy, e)
         test_loss, test_accuracy = self.test()
         os.makedirs(self.out_dir, exist_ok=True)
         torch.save(self.model, self.out_dir+"/model.pt")
