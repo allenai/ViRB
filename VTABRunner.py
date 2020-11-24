c73d219af78052e9366db16721be8bb1d4bd6adb
Klemen Kotar <klemen@kotar.us>
2020-11-24 03:00:25 -0800

    added try except block

--- VTABRunner.py
+++ VTABRunner.py
@@ -155,9 +155,9 @@ class VTABRunner:
             [proc.start() for proc in procs]
             [proc.join() for proc in procs]
         with open("out/results.json", "w+") as f:
-            if os.path.exists("out/results.json"):
+            try:
                 current_results = json.load(f)
-            else:
+            except:
                 current_results = {}
             new_results = current_results + results
             json.dump(new_results)
