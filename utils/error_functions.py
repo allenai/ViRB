583eaaf5dbbd380b1c2ddd8ac71116dfffe756a1
Klemen Kotar <klemen@kotar.us>
2020-11-25 02:11:21 -0800

    Added 8 parallel sun jobs

--- utils/error_functions.py
+++ utils/error_functions.py
@@ -3,5 +3,5 @@ import torch
 
 def classification_error(out, labels):
     with torch.no_grad():
-        _, predictions = torch.max(out, dim=1)
+        predictions = torch.argmax(out, dim=1)
         return (predictions == labels).sum() / labels.size(0)
