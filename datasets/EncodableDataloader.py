d3c5ff845f425dc7f0df0c9bcb4721a84034e4c6
Klemen Kotar <klemen@kotar.us>
2020-11-28 03:54:15 -0800

    Added more code to ensure grads were bnpt being kept where they shouldn't be

--- datasets/EncodableDataloader.py
+++ datasets/EncodableDataloader.py
@@ -9,9 +9,10 @@ class EncodableDataloader:
         label_stack = []
         for d, l in dataloader:
             d = d.to(device)
-            o = model.encoder_forward(d)
-            for name, data_stack in data_stacks.items():
-                data_stack.append(o[name])
+            with torch.no_grad:
+                o = model.encoder_forward(d)
+                for name, data_stack in data_stacks.items():
+                    data_stack.append(o[name])
             label_stack.append(l)
         self.data = {name: torch.cat(data_stacks[name], dim=0).to(device) for name in data_stacks}
         self.labels = torch.cat(label_stack, dim=0).to(device)
@@ -25,7 +26,7 @@ class EncodableDataloader:
         else:
             idxs = torch.randperm(self.__len__()).to(self.device)
         batch_idxs = [idxs[i:min(i+self.batch_size, self.__len__())] for i in range(self.__len__() // self.batch_size)]
-        return [({name: self.data[bi] for name in self.data}, self.labels[bi]) for bi in batch_idxs]
+        return [({name: self.data[bi].detach() for name in self.data}, self.labels[bi]) for bi in batch_idxs]
 
     def __len__(self):
         return self.data.size(0)
