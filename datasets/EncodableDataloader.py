7d2375775ec20ae4e219725b8bdca328b51800d6
Klemen Kotar <klemen@kotar.us>
2020-11-29 20:07:12 -0800

    Fixed iterator

--- datasets/EncodableDataloader.py
+++ datasets/EncodableDataloader.py
@@ -16,13 +16,13 @@ class EncodableDataloader:
                 o = model.encoder_forward(d)
                 for name, data_stack in data_stacks.items():
                     data_stack.append(o[name].detach())
-                total_size = 0
-                for name, data_stack in data_stacks.items():
-                    for tensor in data_stack:
-                        total_size += tensor.element_size() * tensor.nelement()
-                import time
-                time.sleep(10)
-                print("Size of stack: %.4f GB" % (total_size/1.0e9))
+                # total_size = 0
+                # for name, data_stack in data_stacks.items():
+                #     for tensor in data_stack:
+                #         total_size += tensor.element_size() * tensor.nelement()
+                # import time
+                # time.sleep(10)
+                # print("Size of stack: %.4f GB" % (total_size/1.0e9))
             label_stack.append(l)
         self.data = {name: torch.cat(data_stacks[name], dim=0).to(device) for name in data_stacks}
         self.labels = torch.cat(label_stack, dim=0).to(device)
@@ -30,13 +30,13 @@ class EncodableDataloader:
         self.shuffle = shuffle
         self.device = device
 
-    def __call__(self):
+    def __iter__(self):
         if self.shuffle:
             idxs = torch.arange(self.__len__()).to(self.device)
         else:
             idxs = torch.randperm(self.__len__()).to(self.device)
         batch_idxs = [idxs[i:min(i+self.batch_size, self.__len__())] for i in range(self.__len__() // self.batch_size)]
-        return [({name: self.data[bi] for name in self.data}, self.labels[bi]) for bi in batch_idxs]
+        iter([({name: self.data[bi] for name in self.data}, self.labels[bi]) for bi in batch_idxs])
 
     def __len__(self):
         return self.data.size(0)
