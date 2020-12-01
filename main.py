9040a56b0fad12904406c8b1004cc6d3e07e2827
Klemen Kotar <klemen@kotar.us>
2020-11-30 21:13:49 -0800

    Switched main to take args for model and name

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from models.ResNet50Encoder import ResNet50Encoder
 from VTABRunner import VTABRunner
+import sys
 
 
 def main():
     runner = VTABRunner(
-        encoder=ResNet50Encoder(weights="pretrained_weights/SWAV_800.pt"),
-        run_name="SWAV_800",
+        encoder=ResNet50Encoder(weights=sys.argv[2]),
+        run_name=sys.argv[1],
         train_encoder=False,
     )
     runner.run()
