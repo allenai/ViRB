33a4e93b63167ae1db710cd37c5c9750607937d9
Klemen Kotar <klemen@kotar.us>
2020-12-07 03:13:52 -0800

    Added another run where we only store 512 principle ocmponents of the final embedding

--- models/PixelWisePredictionHead.py
+++ models/PixelWisePredictionHead.py
@@ -6,8 +6,8 @@ class PixelWisePredictionHead(nn.Module):
 
     def __init__(self, output_size):
         super().__init__()
-        self.up1 = upshuffle(2048, 1024, 2, kernel_size=3, stride=1, padding=1)
-        self.up2 = upshuffle(1024, 512, 2, kernel_size=3, stride=1, padding=1)
+        self.up1 = upshuffle(512, 512, 2, kernel_size=3, stride=1, padding=1)
+        self.up2 = upshuffle(512, 512, 2, kernel_size=3, stride=1, padding=1)
         self.up3 = upshuffle(512, 256, 2, kernel_size=3, stride=1, padding=1)
         self.up4 = upshuffle(256, 64, 2, kernel_size=3, stride=1, padding=1)
         self.up5 = upshufflenorelu(64, output_size, 2)
@@ -24,32 +24,25 @@ class PixelWisePredictionHead(nn.Module):
         # out = self.up5(d2_)
         # return out
 
-        # d5 = self.up1(x["layer5"].float())
-        # d4 = self.up2(d5 + x["layer4"].float())
-        # d3 = self.up3(d4 + x["layer3"].float())
-        # d2 = self.up4(d3 + x["layer2"].float())
-        # out = self.up5(d2 + x["layer1"].float())
-        # return out
-
         d5 = self.up1(x["layer5"].float())
-        d4 = self.up2(d5)
-        d3 = self.up3(d4)
-        d2 = self.up4(d3)
-        out = self.up5(d2)
+        d4 = self.up2(d5 + x["layer4"].float())
+        d3 = self.up3(d4 + x["layer3"].float())
+        d2 = self.up4(d3 + x["layer2"].float())
+        out = self.up5(d2 + x["layer1"].float())
         return out
 
     def required_encoding(self):
         return ["layer5"]
         # return ["layer1", "layer2", "layer3", "layer4", "layer5"]
 
-    # def pca_embeddings(self):
-    #     return {
-    #         "layer1": 1,
-    #         "layer2": 1,
-    #         "layer3": 1,
-    #         "layer4": 1,
-    #         "layer5": 1024
-    #     }
+    def pca_embeddings(self):
+        return {
+            "layer1": 1,
+            "layer2": 1,
+            "layer3": 1,
+            "layer4": 1,
+            "layer5": 512
+        }
 
 
 def _upsample_add(x, y):
