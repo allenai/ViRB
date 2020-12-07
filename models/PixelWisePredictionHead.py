268ba5c2bedbf1e0cb2ecaad8c730c5d9414af63
Klemen Kotar <klemen@kotar.us>
2020-12-06 19:03:20 -0800

    Set up a run where the pcs dims are annealed from 4 to 32

--- models/PixelWisePredictionHead.py
+++ models/PixelWisePredictionHead.py
@@ -6,11 +6,11 @@ class PixelWisePredictionHead(nn.Module):
 
     def __init__(self, output_size):
         super().__init__()
-        self.up1 = upshuffle(64, 16, 2, kernel_size=3, stride=1, padding=1)
-        self.up2 = upshuffle(16, 16, 2, kernel_size=3, stride=1, padding=1)
-        self.up3 = upshuffle(16, 16, 2, kernel_size=3, stride=1, padding=1)
-        self.up4 = upshuffle(16, 16, 2, kernel_size=3, stride=1, padding=1)
-        self.up5 = upshufflenorelu(16, output_size, 2)
+        self.up1 = upshuffle(128, 32, 2, kernel_size=3, stride=1, padding=1)
+        self.up2 = upshuffle(32, 16, 2, kernel_size=3, stride=1, padding=1)
+        self.up3 = upshuffle(16, 8, 2, kernel_size=3, stride=1, padding=1)
+        self.up4 = upshuffle(8, 4, 2, kernel_size=3, stride=1, padding=1)
+        self.up5 = upshufflenorelu(4, output_size, 2)
 
     def forward(self, x):
         d5 = self.up1(x["layer5"].float())
@@ -36,11 +36,11 @@ class PixelWisePredictionHead(nn.Module):
 
     def pca_embeddings(self):
         return {
-            "layer1": 16,
-            "layer2": 16,
+            "layer1": 4,
+            "layer2": 8,
             "layer3": 16,
-            "layer4": 16,
-            "layer5": 64
+            "layer4": 32,
+            "layer5": 128
         }
 
 
