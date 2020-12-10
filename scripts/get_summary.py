26793e056105350119901f30ba753f5a0f9b4fe0
Klemen Kotar <klemen@kotar.us>
2020-12-10 13:51:21 -0800

    Added better data extraction

--- scripts/get_summary.py
+++ scripts/get_summary.py
@@ -22,11 +22,20 @@ for experiment in glob.glob("../out/*"):
     for task in TASKS:
         training_runs = experiment + "/" + task + "*"
         if len(training_runs) > 0:
-            results[experiment_name][task] = {}
+            results[experiment_name][task] = {"training_runs": {}}
         for training_run in (training_runs):
-            with open(training_run) as f:
+            with open(training_run+"/results.json") as f:
                 run_results = json.load(f)
-            results[experiment_name][task][training_run.replace(task+"-", "")] = run_results
+            results[experiment_name][task]["training_runs"][training_run.replace(task+"-", "")] = run_results
+        if task in results[experiment_name]:
+            best_test_config = None
+            best_test_result = 0.0
+            for name, training_run in results[experiment_name][task]["training_runs"].items():
+                if training_run["test_accuracy"] > best_test_result:
+                    best_test_result = training_run["test_accuracy"]
+                    best_test_config = name
+            results[experiment_name][task]["best_test_config"] = best_test_config
+            results[experiment_name][task]["best_test_result"] = best_test_result
 
 with open("../results.json") as f:
     json.dump(results, f)
