cb70bffeda1b104297470f9b18a91bae2325b837
Klemen Kotar <klemen@kotar.us>
2020-12-10 12:28:30 -0800

    Added config for the rest of vtab

--- tasks/VTABTask.py
+++ tasks/VTABTask.py
@@ -21,7 +21,7 @@ class VTABTask:
             error,
             out_dir,
             scheduler=None,
-            batch_size=32,
+            batch_size=256,
             num_workers=12,
             device="cpu",
             pre_encode=None
