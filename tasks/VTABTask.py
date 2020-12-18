b9c9a8a1a174caa24ce3bf60364a070e5101086d
Klemen Kotar <klemen@kotar.us>
2020-12-17 23:39:16 -0800

    fixed the way principle directions are converted to cpu

--- tasks/VTABTask.py
+++ tasks/VTABTask.py
@@ -97,8 +97,10 @@ class VTABTask:
             with open(out_dir+"/results.json", "w") as f:
                 json.dump(data, f)
             torch.save(config["model"].task_head.state_dict(), out_dir+"/model_head.pt")
+            principle_directions = self.train_dataloader.get_principal_directions()
+            cpu_principle_directions = {name: pd.detach().cpu() for name, pd in principle_directions.items()}
             with open(out_dir+"/principle_directions.pkl", "wb") as f:
-                pickle.dump(self.train_dataloader.get_principal_directions().detach().cpu(), f)
+                pickle.dump(cpu_principle_directions, f)
 
     def train_epoch(self, model, optimizer):
         model.train()
