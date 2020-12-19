25e455b8cfb1299aa4a0e494372b4c35a667b502
Klemen Kotar <klemen@kotar.us>
2020-12-19 00:27:55 -0800

    Fixed nyu dataset

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -6,10 +6,12 @@ import random
 path = sys.argv[1]
 os.makedirs(path+"test/", exist_ok=True)
 cats = glob.glob(path + "train/*")
+
+imgs = [im.split("/")[-1] for im in glob.glob(cats[0]+"/*.png")]
+random.shuffle(imgs)
+split = imgs[int(0.8*len(imgs)):]
+
 for cat in cats:
     os.makedirs(cat.replace("train", "test"), exist_ok=True)
-    imgs = glob.glob(cat+"/*.png")
-    random.shuffle(imgs)
-    split = imgs[int(0.8*len(imgs)):]
     for img in split:
-        os.rename(img, img.replace("train", "test"))
+        os.rename(cat + "/" + img, cat.replace("train", "test") + "/" + img)
