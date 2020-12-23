4631d25dc0042779ec1f00e0f6e5779f9b09f53b
Klemen Kotar <klemen@kotar.us>
2020-12-23 10:38:53 -0800

    Added print debugging

--- VTABRunner.py
+++ VTABRunner.py
@@ -149,11 +149,16 @@ def get_error_function(config):
 
 
 def run_VTAB_task(config, logging_queue):
+    print("1")
     dataset_class = get_dataset_class(config)
+    print("2")
     trainset = dataset_class(train=True)
     testset = dataset_class(train=False)
+    print("3")
     loss_function = get_loss_function(config)
+    print("4")
     error_function = get_error_function(config)
+    print("5")
     training_configs = []
     for tc_name, tc in config["training_configs"].items():
         encoder = copy.deepcopy(config["encoder"])
@@ -167,8 +172,10 @@ def run_VTAB_task(config, logging_queue):
             "optimizer": optimizer,
             "scheduler": scheduler
         })
+    print("6")
     pre_encode = config["pre_encode"] if "pre_encode" in config else None
     num_dataset_repeats = config["num_dataset_repeats"] if "num_dataset_repeats" in config else 1
+    print("7")
     task = VTABTask(
         config["experiment_name"],
         config["task_name"],
@@ -184,13 +191,16 @@ def run_VTAB_task(config, logging_queue):
         pre_encode=pre_encode,
         num_dataset_repeats=num_dataset_repeats,
     )
+    print("8")
     results = task.run(config["num_epochs"])
+    print("9")
     return results
 
 
 def thread_loop(gpu_id, config_queue, logging_queue):
     while True:
         try:
+            print("A")
             conf = config_queue.get(timeout=1)
             conf["device_id"] = gpu_id
             logging_queue.put(ProgressDataPacket(
@@ -198,6 +208,7 @@ def thread_loop(gpu_id, config_queue, logging_queue):
                 device=gpu_id,
                 new_task=False
             ))
+            print("B")
             run_VTAB_task(conf, logging_queue)
             logging_queue.put(ProgressDataPacket(
                 name="Done",
