51a7e39e220d5c2bda4e00f0e828f96f6caac4d0
Klemen Kotar <klemen@kotar.us>
2021-01-05 11:50:08 -0800

    Added part of graph making script

---
+++ scripts/make_charts.py
@@ -0,0 +1,17 @@
+import json
+import glob
+import matplotlib.pyplot as plt
+
+
+def get_best_result(experiments, run):
+    res = []
+    for e in experiments:
+        datapoints = []
+        datapoint_filels = glob.glob("%s/%s*/results.json" % (e, run))
+        for f in datapoint_filels:
+            with open(f) as fp:
+                datapoints.append(float(json.load(fp)["test_accuracy"]))
+        res.append((e, max(datapoints)))
+    return res
+
+print(get_best_result(["Supervised", "Random", "SWAV_800"], "TaskonomyInpainting"))
