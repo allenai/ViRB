dc28ef777946cdd23a3c62b872b214917c16b34c
Klemen Kotar <klemen@kotar.us>
2021-01-05 17:20:25 -0800

    Added config for longer training run of inpainting

--- scripts/make_charts.py
+++ scripts/make_charts.py
@@ -19,4 +19,17 @@ def get_best_result(experiments, run, include_names=False):
             res.append(max(datapoints))
     return res
 
-print(get_best_result(["Random-full-channel", "Supervised-full-channel"], "TaskonomyInpainting"))
+
+# #### Taskonomy Inpainting number of channels
+# RUNS = ["Random", "Supervised", "SWAV_800", "MoCov2_800", "PIRL", "MoCov2_200", "MoCov2Taskonomy", "SWAVTaskonomy"]
+# end_to_end = get_best_result([r+"-end-to-end" for r in RUNS], "TaskonomyInpainting")
+# plt.scatter(["End to End (1 hour)"]*len(end_to_end), end_to_end)
+# full_channel = get_best_result([r+"-full-channel" for r in RUNS], "TaskonomyInpainting")
+# plt.scatter(["Full Channel (53 minutes)"]*len(full_channel), full_channel)
+# encoded_2_128 = get_best_result([r+"-2-128" for r in RUNS], "TaskonomyInpainting")
+# plt.scatter(["2-128 (43 minutes)"]*len(encoded_2_128), encoded_2_128)
+# encoded_4_128 = get_best_result([r+"-4-128" for r in RUNS], "TaskonomyInpainting")
+# plt.scatter(["4-128 (45 minutes)"]*len(encoded_4_128), encoded_4_128)
+# plt.show()
+
+
