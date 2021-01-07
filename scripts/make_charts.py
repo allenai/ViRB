7e30c48215a6106420598e370dce97a0d90451c5
Klemen Kotar <klemen@kotar.us>
2021-01-07 10:41:49 -0800

    set up pets detection full-channel run

--- scripts/make_charts.py
+++ scripts/make_charts.py
@@ -1,7 +1,42 @@
 import json
 import glob
+import csv
 import matplotlib.pyplot as plt
 
+
+ALL_EXPERIMENTS = [
+    'Random',
+    'Supervised',
+    'SWAV_800',
+    'MoCov2_800',
+    'MoCov1_200',
+    'PIRL',
+    'SimCLR_1000',
+    'MoCov2_200',
+    'MoCov2Combination',
+    'MoCov2Taskonomy',
+    'MoCov2Kinetics',
+    'MoCov2Places',
+    'MoCov2HalfImagenet',
+    'MoCov2LogImagenet',
+    'SWAV_200',
+    'SWAVCombination',
+    'SWAVTaskonomy',
+    'SWAVKinetics',
+    'SWAVPlaces',
+    'SWAVHalfImagenet',
+    'SWAVLogImagenet'
+]
+
+ALL_TASKS = [
+    "Pets",
+    "SUN397",
+    "CIFAR-100",
+    "CalTech-101",
+    "Eurosat",
+    "dtd",
+    "CLEVERNumObjects"
+]
 
 def get_best_result(experiments, run, include_names=False):
     res = []
@@ -20,7 +55,7 @@ def get_best_result(experiments, run, include_names=False):
     return res
 
 
-# #### Taskonomy Inpainting number of channels
+#### Taskonomy Inpainting number of channels
 # RUNS = ["Random", "Supervised", "SWAV_800", "MoCov2_800", "PIRL", "MoCov2_200", "MoCov2Taskonomy", "SWAVTaskonomy"]
 # end_to_end = get_best_result([r+"-end-to-end" for r in RUNS], "TaskonomyInpainting")
 # plt.scatter(["End to End (1 hour)"]*len(end_to_end), end_to_end)
@@ -32,4 +67,18 @@ def get_best_result(experiments, run, include_names=False):
 # plt.scatter(["4-128 (45 minutes)"]*len(encoded_4_128), encoded_4_128)
 # plt.show()
 
+#### Converting the output to csv format
+experiment_results = {name: {} for name in ALL_EXPERIMENTS}
+for task in ALL_TASKS:
+    res = get_best_result(ALL_EXPERIMENTS, task, include_names=True)
+    for name, number in res:
+        experiment_results[name][task] = number
+with open('results.csv', mode='w') as csv_file:
+    fieldnames = ["Encoder"] + ALL_TASKS
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for name, results in experiment_results.items():
+        row = {"Encoder": name}
+        row.update(results)
+        writer.writerow(row)
 
