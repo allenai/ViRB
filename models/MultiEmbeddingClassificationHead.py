f218ccd194244afe0f4cda16acd3ad6cb8ccb3e9
Klemen Kotar <klemen@kotar.us>
2021-01-23 02:25:18 -0800

    Added extra layer for embedding fusion to multi embeddgin classification head

--- models/MultiEmbeddingClassificationHead.py
+++ models/MultiEmbeddingClassificationHead.py
@@ -6,14 +6,12 @@ class MultiEmbeddingClassificationHead(nn.Module):
     def __init__(self, embedding_size, output_size, num_embeddings):
         super().__init__()
         self.embedding_size = embedding_size
-        # self.head = nn.Sequential(
-        #     nn.Linear(embedding_size, 256),
-        #     nn.ReLU(inplace=True),
-        #     nn.Linear(256, 256),
-        #     nn.ReLU(inplace=True),
-        #     nn.Linear(256, output_size)
-        # )
-        self.head = nn.Linear(embedding_size * num_embeddings, output_size)
+        self.head = nn.Sequential(
+            nn.Linear(embedding_size * num_embeddings, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, output_size)
+        )
+        # self.head = nn.Linear(embedding_size * num_embeddings, output_size)
 
     def forward(self, x):
         x = x["embedding"].float()
