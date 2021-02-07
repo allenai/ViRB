6173b155938236ea43c9f0618917e4045323171a
Klemen Kotar <klemen@kotar.us>
2021-02-07 13:07:19 -0800

    Fixed bug inout prune script

--- scripts/prune_out.py
+++ scripts/prune_out.py
@@ -9,4 +9,4 @@ for dir in glob.glob("../out/*/*"):
     tbs.sort(key=lambda x: x[1], reverse=True)
     if len(tbs) > 1 and tbs[0][1] > tbs[1][1]:
         for bad_tb in tbs[1:]:
-            os.rmdir(bad_tb[0])
+            os.remove(bad_tb[0])
