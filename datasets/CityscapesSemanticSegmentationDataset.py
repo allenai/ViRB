3d787076f3bc57cd03b92395c3b3e09457966931
Klemen Kotar <klemen@kotar.us>
2021-02-08 13:09:07 -0800

    Fixed label upsampling

--- datasets/CityscapesSemanticSegmentationDataset.py
+++ datasets/CityscapesSemanticSegmentationDataset.py
@@ -102,7 +102,7 @@ class CityscapesSemanticSegmentationDataset:
             # Sclae the image
             scale = random.uniform(0.5, 2.0)
             img = img.resize((int(ogw * scale), int(ogh * scale)))
-            label = label.resize((int(ogw * scale), int(ogh * scale)))
+            label = label.resize((int(ogw * scale), int(ogh * scale)), resample=Image.NEAREST)
 
             # Mirror the image half of the time
             if random.uniform(0, 1) > 0.5:
