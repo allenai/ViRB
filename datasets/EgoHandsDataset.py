91826a8fac9f4435eee94536aa9861d089811566
Klemen Kotar <klemen@kotar.us>
2021-02-12 18:26:12 -0800

    Switched back to bs=16 and crop=513

--- datasets/EgoHandsDataset.py
+++ datasets/EgoHandsDataset.py
@@ -46,42 +46,40 @@ class EgoHandsDataset:
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        # if self.train:
-        #     i = Image.open(self.imgs[idx]).convert('RGB')
-        #     l = Image.open(self.labels[idx]).convert('I')
-        #     ogw, ogh = i.size
-        #
-        #     # # Scale the image
-        #     # scale = random.uniform(0.5, 2.0)
-        #     # img = img.resize((int(ogw * scale), int(ogh * scale)))
-        #     # label = label.resize((int(ogw * scale), int(ogh * scale)), resample=Image.NEAREST)
-        #     #
-        #     # # Mirror the image half of the time
-        #     # if random.uniform(0, 1) > 0.5:
-        #     #     img = ImageOps.mirror(img)
-        #     #     label = ImageOps.mirror(label)
-        #
-        #     # Add random crop to image
-        #     while True:
-        #         cw = 450
-        #         ch = 450
-        #         x = random.randint(0, ogw - cw)
-        #         y = random.randint(0, ogh - ch)
-        #         img = i.crop((x, y, x+cw, y+ch))
-        #         label = l.crop((x, y, x+cw, y+ch))
-        #
-        #         img = self.img_preprocessor(img)
-        #         label = self.label_preprocessor(label).long().squeeze()
-        #
-        #         if torch.unique(label).shape[0] > 5:
-        #             print("PROBLEM<!!!")
-        #
-        #         if torch.unique(label).shape[0] > 1:
-        #             break
-        #
-        # else:
-        img = self.img_preprocessor(Image.open(self.imgs[idx]).convert('RGB'))
-        label = self.label_preprocessor(Image.open(self.labels[idx]).convert('I')).long().squeeze()
+        if self.train:
+            i = Image.open(self.imgs[idx]).convert('RGB')
+            l = Image.open(self.labels[idx]).convert('I')
+            ogw, ogh = i.size
+
+            # # Scale the image
+            # scale = random.uniform(0.5, 2.0)
+            # img = img.resize((int(ogw * scale), int(ogh * scale)))
+            # label = label.resize((int(ogw * scale), int(ogh * scale)), resample=Image.NEAREST)
+            #
+            # # Mirror the image half of the time
+            # if random.uniform(0, 1) > 0.5:
+            #     img = ImageOps.mirror(img)
+            #     label = ImageOps.mirror(label)
+
+            # Add random crop to image
+            while True:
+                cw = 513
+                ch = 513
+                x = random.randint(0, ogw - cw)
+                y = random.randint(0, ogh - ch)
+                img = i.crop((x, y, x+cw, y+ch))
+                label = l.crop((x, y, x+cw, y+ch))
+
+                img = self.img_preprocessor(img)
+                label = self.label_preprocessor(label).long().squeeze()
+
+                if torch.unique(label).shape[0] > 1:
+                    break
+
+        else:
+            img = self.img_preprocessor(Image.open(self.imgs[idx]).convert('RGB'))
+            label = self.label_preprocessor(Image.open(self.labels[idx]).convert('I')).long().squeeze()
+
         return img, label
 
     def __len__(self):
