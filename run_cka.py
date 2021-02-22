32dcc950d6b0750b87eda2a6528eb44d18a016f2
Klemen Kotar <klemen@kotar.us>
2021-02-22 03:03:23 -0800

    Moved cka compute to cpu

--- run_cka.py
+++ run_cka.py
@@ -150,7 +150,7 @@ for weights in glob.glob("pretrained_weights/*.pt"):
         outs[key] = torch.cat(outs[key], dim=0)
     cka = compute_cka(outs["embedding"])
     print("Processing", weights.split("/")[1].split(".")[0], torch.linalg.norm(outs["embedding"], ord=1).item())
-    model_similarity[weights.split("/")[1].split(".")[0]] = cka
+    model_similarity[weights.split("/")[1].split(".")[0]] = cka.cpu()
 
 cka_table = torch.ones((len(model_similarity), len(model_similarity)))
 cs = torch.nn.CosineSimilarity(dim=0)
