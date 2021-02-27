a2551a0299a17500d3e0c62923e6cc9f84a1d530
Klemen Kotar <klemen@kotar.us>
2021-02-27 13:51:26 -0800

    Added more logging

--- run_cka.py
+++ run_cka.py
@@ -217,6 +217,7 @@ def linear_cka(dataset):
         for j in range(i, n):
             x = data[keys[i]].to(device)
             y = data[keys[j]].to(device)
+            print((torch.norm(y.T @ x) ** 2).item(), torch.norm(x.T @ x).item(), torch.norm(y.T @ y).item())
             cka = (torch.norm(y.T @ x) ** 2) / (torch.norm(x.T @ x) * torch.norm(y.T @ y))
             heatmap[i, j] = heatmap[j, i] = cka
     plt.figure(figsize=(20, 15))
