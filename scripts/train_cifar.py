34afb142b46b30f2ad681233f5182d92ff43c8f3
Klemen Kotar <klemen@kotar.us>
2021-03-08 16:23:27 -0800

    fixed sizing bug

--- scripts/train_cifar.py
+++ scripts/train_cifar.py
@@ -120,7 +120,7 @@ class ResNet50Encoder(nn.Module):
         return res
 
 
-def fro_matmul(a, b, istride=100000, jstride=6000, device="cpu"):
+def fro_matmul(a, b, istride=100000, jstride=5000, device="cpu"):
     s = 0.0
     print(a.shape, b.shape)
     with torch.no_grad():
